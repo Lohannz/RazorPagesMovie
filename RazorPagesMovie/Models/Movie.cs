2bb574a76b9070c20455af01440ec0ae2b5eb415
lohan <lohan@DESKTOP-79UO6MB>
2022-12-11 15:17:10 -0300

    Adicionar arquivos de projeto.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    // Title
+    [StringLength(60, MinimumLength =2)]
+    [Required]
+    public string? Title { get; set; } = string.Empty;
+
+    // Rea
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(40)]
+    public string? Genre { get; set; } = string.Empty;
+
+
+    [DataType(DataType.Currency)]
+    [Range(1,100)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
+    public string Rating { get; set; } = string.Empty;
+}
